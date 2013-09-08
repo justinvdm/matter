6ca8829029688d04ef12912b36134843af482c50
justinvdm <justinvdm.0.1.0@gmail.com>
2013-09-08 11:05:22 +0200

    Make the api a bit more sane

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 var matter = exports;
 
 matter.parsers = require('./parsers');
-matter.register = matter.parsers.register;
-matter.defaults = matter.parsers.defaults;
 matter.parse = matter.parsers.registry;
 
 // register the bundled parsers
